31c5da6d2e5c0918d0d7977a053a201422f847bc
ridma <ridma@ceylonlinux.lk>
2023-01-16 19:59:56 +0530

    new

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const selectHeader = document.querySelector('#header');
   if (selectHeader) {
     document.addEventListener('scroll', () => {
-      window.scrollY > 100 ? selectHeader.classList.add('sticked') : selectHeader.classList.remove('sticked');
+      window.scrollY > 80 ? selectHeader.classList.add('sticked') : selectHeader.classList.remove('sticked');
     });
   }
 
